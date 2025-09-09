3a23e062fdc593840dfc25b277f92ac77f2baef7
jesus-192 <jesusvazquez732@gmail.com>
2025-09-09 15:13:40 -0600

    validar que sea png
    pip install Pillow

--- models/product.py
+++ models/product.py
@@ -2,6 +2,9 @@
 
 from odoo import fields, models, api, _
 from odoo.exceptions import ValidationError
+from io import BytesIO
+from PIL import Image
+import base64
 
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
@@ -21,3 +24,16 @@ class ProductTemplate(models.Model):
                     raise ValidationError(_(
                         "La imagen para la aplicación excede el tamaño máximo permitido de %s KB." % record._max_image_size_kb
                     ))
+
+    @api.constrains('imagen_app')
+    def _check_imagen_app_format(self):
+        for record in self:
+            if record.imagen_app:
+                try:
+                    # Odoo almacena binarios como base64, necesitamos decodificarlo para Pillow
+                    image_bytes = base64.b64decode(record.imagen_app)
+                    image = Image.open(BytesIO(image_bytes))
+                    if image.format.upper() != 'PNG':
+                        raise ValidationError(_("La imagen para la aplicación debe ser en formato PNG."))
+                except (IOError, Image.UnidentifiedImageError):
+                    raise ValidationError(_("No se pudo identificar el formato de la imagen. Asegúrate de que sea un archivo de imagen válido."))
